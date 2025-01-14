31ac635edc2c95ecd6ec7c5d6a56e74d44ce191d
Isaiah <isaiahmstockton@gmail.com>
2025-01-14 18:24:55 -0500

    feat: implement nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex flex-col">
+	<nav class="container flex items-center justify-between bg-background py-2">
+		<a href="/" class="flex items-center gap-1">
+			<img src="/favicon.ico" alt="logo" class="h-12 w-12" />
+			<p class="text-2xl">Music Wizard</p>
+		</a>
+		<div>
+			<Button disabled>Login</Button>
+		</div>
+	</nav>
+	<main class="flex-1">
+		{@render children()}
+	</main>
+</div>
