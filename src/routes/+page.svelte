37dfc8870a1a4fef7b9a6eb672b8e9d07402cfc5
Isaiah <isaiahmstockton@gmail.com>
2025-01-14 18:41:39 -0500

    feat: add landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<svelte:head>
+	<title>Music Wizard</title>
+</svelte:head>
+
+<div class="flex h-full flex-col items-center justify-center gap-2 bg-[url('/herobanner.svg')]">
+	<div class="text-center drop-shadow-md">
+		<h1 class="text-3xl font-bold">Uncover your music</h1>
+		<p class="text-lg">Dive into what makes your music unique</p>
+	</div>
+	<Button disabled>Get Started</Button>
+</div>
